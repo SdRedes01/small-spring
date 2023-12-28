e5e8b4e7ec30bee230252f66a99d903e2f012bff
SdRedes01 <154896224+SdRedes01@users.noreply.github.com>
2023-12-28 14:46:18 -0300

    Update Advisor.java

--- small-spring-step-13/src/main/java/cn/bugstack/springframework/aop/Advisor.java
+++ small-spring-step-13/src/main/java/cn/bugstack/springframework/aop/Advisor.java
@@ -6,7 +6,7 @@ import org.aopalliance.aop.Advice;
  * Base interface holding AOP <b>advice</b> (action to take at a joinpoint)
  * and a filter determining the applicability of the advice (such as
  * a pointcut). <i>This interface is not for use by Spring users, but to
- * allow for commonality in support for different types of advice.</i>
+ * allow for commonality in support for different types of advice./i
  *
  *
  *
